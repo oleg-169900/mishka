dd1cd9b3b8d8613a5ec36844fbaa71abc15e7404
oleg-169900 <oleg-169900@yandex.ru>
2018-04-18 18:00:58 +0300

    Сделал main

---
+++ js/menu-toggle.js
@@ -0,0 +1,18 @@
+"use strict";
+
+(function () {
+  var navMain = document.querySelector(".main-nav");
+  var navToggle = document.querySelector(".menu-toggle");
+
+  navMain.classList.remove("main-nav--nojs");
+
+  navToggle.addEventListener("click", function() {
+    if (navMain.classList.contains("hidden")) {
+      navMain.classList.remove("hidden");
+      navToggle.classList.add("menu-toggle--close");
+    } else {
+      navMain.classList.add("hidden");
+      navToggle.classList.remove("menu-toggle--close");
+    }
+  });
+})();
